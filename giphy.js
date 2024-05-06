6d05916e383f81b36626dd937857dac7483570d0
Williams439 <andrewpwilliams1776@gmail.com>
2024-05-06 16:41:25 -0400

    giphy server data fetch

---
+++ giphy.js
@@ -0,0 +1,24 @@
+const API_KEY = "uA36R7Po1mE3xuOSsrCXIlmsI9PKtIIu"
+const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
+const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+function renderGifs(response) {
+    console.log(response);
+}
+
+function getMemes(searchExpression) {
+    fetch(
+        '${API_PREFIX}${API_KEY}u&q=${searchExpression}&limit=25${API_SETTINGS}'
+    )
+        .then(data => data.json())
+        .then(renderGifs);
+}
+
+function formSubmitted(event) {
+    event.preventDefault();
+    let inputFieldContent = document.querySelector("[name=meme-input]").value;
+    getMemes(inputFieldContent);
+    
+}
+
+document.querySelector("#meme-form").addEventListener("submit", formSubmitted);
