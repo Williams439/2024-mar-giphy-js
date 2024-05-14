491a8f97fd3b737d1764704ca4ef0898e7e31b5f
Williams439 <andrewpwilliams1776@gmail.com>
2024-05-14 11:40:01 -0400

    pagination

--- giphy.js
+++ giphy.js
@@ -2,9 +2,72 @@ const API_KEY = "uA36R7Po1mE3xuOSsrCXIlmsI9PKtIIu";
 const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
 const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
+/* Application interal State */
+let memesPerPage = 1;
+let currentPage = 1;
+
+function generatePaginationListItems(pageCount) {
+
+    let startClass = '';
+    let endClass = '';
+    let previousClass = '';
+    let nextClass = '';
+
+    if (pageCount === 1) {
+        startClass = 'disabled';
+        endClass = 'disabled';
+        previousClass = 'disabled';
+        nextClass = 'disabled';
+
+    }
+
+    if (currentPAge === 1) {
+        previousClass = 'disabled';
+        startClass = 'disabled';
+    }
+
+    if (pageCount === currentPage) {
+        endClass = 'disabled';
+        nextClass = 'disabled';
+    }
+
+    return `
+    <li class="page-item" "${startClass}">
+        <a class="page-link" href="#">Start</a>
+    </li>
+    <li class="page-item" "${previousClass}">
+        <a class="page-link" href="#">Previous</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item" ${nextClass}">
+        <a class="page-link" href="#">Next</a>
+    </li>
+    <li class="page-item" ${endClass}">
+        <a class="page-link" href="#">End</a>
+    </li>
+    `;
+}
+
 function renderPagination(paginationInfo) {
-    let {count, offset, total_count: totalCount} = paginationInfo;
-    console.log(count, offset, totalCount);
+    let { count, offset, total_count: totalCount } = paginationInfo;
+    let pageCount = Math.ceil( totalCount / count );
+    let currentPage = Math.floor( offset / count ) + 1;
+
+    let html = `
+<nav 
+    aria-label="Page navigation example" 
+    data-bs-theme="dark"
+    class="pagination-component">
+  <ul class="pagination">
+    ${generatePaginationListItems(pageCount)}
+    <li class="page-label">Page ${currentPage} of ${pageCount}</li>
+  </ul>
+</nav>`;
+
+    document.querySelector(".js-pagination-top").innerHTML = html;
+    document.querySelector(".js-pagination-bottom").innerHTML = html;
 }
 
 function renderGifs(response) {
@@ -34,9 +97,9 @@ function renderError(message) {
         `<div class="alert alert-danger error-container">${message}</div>`;
 }
 
-function getMemes(searchExpression, memeCount) {
+function getMemes(searchExpression) {
     fetch(
-        `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=${memeCount}${API_SETTINGS}`
+        `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=${memesPerPage}${API_SETTINGS}`
     )
         .then(data => data.json())
         .then(renderGifs)
@@ -46,8 +109,12 @@ function getMemes(searchExpression, memeCount) {
 function formSubmitted(event) {
     event.preventDefault();
     let inputFieldContent = document.querySelector("[name=meme-input]").value;
-    let memecount = document.querySelector("[name=meme-count]").value;
-    getMemes(inputFieldContent, memecount);
+
+
+    memesPerPage = document.querySelector("[name=meme-count]").value;
+    currentPage = 1;
+
+    getMemes(inputFieldContent);
 }
 
 document.querySelector("#meme-form").addEventListener("submit", formSubmitted);
